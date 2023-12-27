ccb78369834d6622f5b4fc3c79ed1f203b589e74
NullBite <me@nullbite.com>
2023-12-27 02:37:46 -0500

    Add initial flake
    holy shit nixos is magic :3

---
+++ flake.nix
@@ -0,0 +1,20 @@
+{
+  description = "NixOS Configuration";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+                 # ^^^^^^^^^^^^^ this part is optional
+  };
+
+  outputs = { self, nixpkgs }: 
+  let
+    lib = nixpkgs.lib;
+  in {
+    nixosConfigurations = {
+      slab = lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [ ./configuration.nix ];
+      };
+    };
+  };
+}
