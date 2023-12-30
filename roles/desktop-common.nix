c75fd66f05bde5b2f818acc095eecf7c7b019748
NullBite <me@nullbite.com>
2023-12-30 11:03:23 -0500

    Split common configurations from desktop.nix

---
+++ roles/desktop-common.nix
@@ -0,0 +1,30 @@
+{ config, lib, pkgs, ...}:
+
+{
+  imports = [
+    ./base.nix
+    ../fragments/sound.nix
+    ../fragments/multimedia.nix
+    ../fragments/software/syncthing.nix
+    ../fragments/hardware/bluetooth.nix
+  ];
+  
+  # Enable the X11 windowing system.
+  services.xserver.enable = true;
+
+  environment.systemPackages = with pkgs; [
+    arc-theme
+  ];
+
+  # Enable flatpak
+  services.flatpak.enable = true;
+
+  # Enable CUPS to print documents.
+  services.printing.enable = true;
+
+  users.users.nullbite = {
+    packages = with pkgs; [
+      firefox
+    ];
+  };
+}
--- roles/desktop.nix
+++ roles/desktop.nix
@@ -2,32 +2,9 @@
 
 {
   imports = [
-    ./base.nix
-    ../fragments/sound.nix
-    ../fragments/multimedia.nix
-    ../fragments/software/syncthing.nix
-    ../fragments/hardware/bluetooth.nix
+    ./desktop-common.nix
   ];
-  
-  # Enable the X11 windowing system.
-  services.xserver.enable = true;
 
   services.xserver.displayManager.sddm.enable = true;
   services.xserver.desktopManager.plasma5.enable = true;
-
-  environment.systemPackages = with pkgs; [
-    arc-theme
-  ];
-
-  # Enable flatpak
-  services.flatpak.enable = true;
-
-  # Enable CUPS to print documents.
-  services.printing.enable = true;
-
-  users.users.nullbite = {
-    packages = with pkgs; [
-      firefox
-    ];
-  };
 }
