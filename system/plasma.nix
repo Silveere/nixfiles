7e81f1b022328b311e969ea374f9b7f33ec219db
NullBite <me@nullbite.com>
2024-01-22 00:34:02 +0100

    Make plasma wayland the default plasma session

--- system/plasma.nix
+++ system/plasma.nix
@@ -7,6 +7,7 @@
 
   services.xserver.displayManager.sddm.enable = true;
   services.xserver.desktopManager.plasma5.enable = true;
+  services.xserver.displayManager.defaultSession = "plasmawayland";
   programs.kdeconnect.enable = true;
 
   environment.systemPackages = with pkgs; [
