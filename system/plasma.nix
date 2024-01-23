09da72a0bf42248b8cff62c6a73fb87f7d94424f
NullBite <me@nullbite.com>
2024-01-23 12:41:21 +0100

    add KDE XDG desktop portal workaround

--- system/plasma.nix
+++ system/plasma.nix
@@ -1,5 +1,8 @@
 { config, lib, pkgs, ...}:
-
+let
+  sleep = "${pkgs.coreutils}/bin/sleep";
+  systemctl = "${pkgs.systemd}/bin/systemctl";
+in
 {
   imports = [
     ./desktop-common.nix
@@ -9,6 +12,26 @@
   services.xserver.desktopManager.plasma5.enable = true;
   services.xserver.displayManager.defaultSession = "plasmawayland";
   programs.kdeconnect.enable = true;
+
+  # systemd.user.targets.plasma-core.unitConfig.Upholds = [ "restart-xdg-desktop-portal.service" ];
+  systemd.user.targets.graphical-session.unitConfig.Wants = [ "restart-xdg-desktop-portal-kde.service" ];
+  systemd.user = {
+    services.restart-xdg-desktop-portal-kde = {
+      enable = true;
+      description = "hack to fix xdg-desktop-portal on kde";
+      # wantedBy = [ "plasma-core.target" ];
+      after = [ "plasma-core.target" "xdg-desktop-portal.service" ];
+      requisite = [ "plasma-core.target" ];
+      serviceConfig = {
+        ExecStart = [
+          "${sleep} 5"
+          "${systemctl} --user restart xdg-desktop-portal.service"
+        ];
+        Type = "oneshot";
+        RemainAfterExit = "yes";
+      };
+    };
+  };
 
   environment.systemPackages = with pkgs; [
     # this fixes tiny file dialogs for Minecraft
