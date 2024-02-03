808c759f893fc4e4690127ef236e2686af3fe5e7
NullBite <me@nullbite.com>
2024-02-03 18:33:19 +0100

    hyprland: implement brightness

--- home/wm-helpers.nix
+++ home/wm-helpers.nix
@@ -4,6 +4,7 @@
   keysetting = pkgs.writeShellScript "keysetting" ''
     wpctl=${pkgs.wireplumber}/bin/wpctl
     notify_send=${pkgs.libnotify}/bin/notify-send
+    brightnessctl=${pkgs.brightnessctl}/bin/brightnessctl
 
     notify-send () {
       $notify_send -h string:x-canonical-private-synchronous:keysetting "$@"
@@ -17,10 +18,21 @@
       $wpctl set-volume @DEFAULT_SINK@ "$1"
       notifyvol
     }
+
+    notifybright () {
+      notify-send "Brightness: $(($($brightnessctl g)*100/$($brightnessctl m)))%"
+    }
+
+    setbright () {
+      $brightnessctl s "$1"
+      notifybright
+    }
     case "$1" in
       volumeup) setvol 5%+ ;;
       volumedown) setvol 5%- ;;
       mute) $wpctl set-mute @DEFAULT_SINK@ toggle; notifyvol;;
+      monup) setbright 5%+;;
+      mondown) setbright 5%-;;
     esac
   '';
 }
