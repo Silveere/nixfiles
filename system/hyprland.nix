73208c3be0dc23946b333caed439275bbab8def2
NullBite <me@nullbite.com>
2024-02-03 18:32:40 +0100

    hyprland: enable sddm

--- system/hyprland.nix
+++ system/hyprland.nix
@@ -4,10 +4,19 @@
     ./desktop-common.nix
   ];
 
+  services.xserver.displayManager.sddm.enable = true;
+
   programs.hyprland = {
     enable = true;
     enableNvidiaPatches = true;
     xwayland.enable = true;
+  };
+
+  xdg.portal = {
+    enable = true;
+    extraPortals = [
+      pkgs.xdg-desktop-portal-gtk
+    ];
   };
 
   environment.systemPackages = with pkgs; [
