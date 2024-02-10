ae88738a968f6b92d77885682079c57099d65308
NullBite <me@nullbite.com>
2024-02-10 23:15:31 +0100

    Update android configuration

--- system/fragments/android.nix
+++ system/fragments/android.nix
@@ -1,8 +1,15 @@
 { config, lib, pkgs, outputs, vars, ...}@args:
+let
+  cfg = config.nixfiles.programs.adb;
+in
 {
-  imports = [ outputs.nixosModules.adb ];
-  
-  config = {
+  # imports = [ outputs.nixosModules.adb ];
+
+  options.nixfiles.programs.adb = {
+    enable = lib.mkEnableOption "adb configuration";
+  };
+
+  config = lib.mkIf cfg.enable {
     programs.adb.enable = true;
     users.users.${vars.username}.extraGroups = [ "adbusers" ];
   };
