2ce393d1aec51527824fde5c7680214f6d2b2b67
NullBite <me@nullbite.com>
2024-02-15 13:59:44 +0100

    Add /.btrfsroot to compressed mounts

--- hosts/slab/configuration.nix
+++ hosts/slab/configuration.nix
@@ -19,7 +19,7 @@
       };
     }
 
-    (lib.genAttrs [ "/" "/home" "/nix" ] ( fs: {
+    (lib.genAttrs [ "/.btrfsroot" "/" "/home" "/nix" ] ( fs: {
       options = [ "compress=zstd" ];
     }))
   ];
