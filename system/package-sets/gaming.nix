91462a3a15c62c7d4ed4ebd8666141a657779273
NullBite <me@nullbite.com>
2024-02-16 16:38:45 +0100

    Add workaround for steam breaking on rebuild

--- system/package-sets/gaming.nix
+++ system/package-sets/gaming.nix
@@ -1,6 +1,7 @@
-{ config, lib, pkgs, ...}:
+{ config, lib, pkgs, inputs, ...}:
 let
   cfg = config.nixfiles.packageSets.gaming;
+  is2311 = lib.strings.hasInfix "23.11" lib.version;
 in
 {
   # oopsies this is for home-manager
@@ -10,6 +11,13 @@ in
     enable = lib.mkEnableOption "gaming package set";
   };
   config = lib.mkIf cfg.enable {
+
+                                  # only needed on 23.11, increases closure size MASSIVELY
+    nixpkgs.overlays = lib.optional is2311 (_: _: {
+      # unstable steam has new buildFSHEnv which doesn't break on rebuild
+      steam = (import inputs.nixpkgs-unstable.outPath {config.allowUnfree = true; inherit (pkgs) system; }).steam;
+    });
+
     programs.steam = {
       enable = lib.mkDefault true;
       gamescopeSession = {
