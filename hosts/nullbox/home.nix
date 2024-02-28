4e6889e5b12e5f37205e79f0c151271d1cd1ecff
NullBite <me@nullbite.com>
2024-02-28 16:14:40 -0500

    nullbox: configure monitors under hyprland

--- hosts/nullbox/home.nix
+++ hosts/nullbox/home.nix
@@ -7,5 +7,12 @@
     nixfiles.profile.base.enable = true;
 
     home.stateVersion = "23.11";
+
+    wayland.windowManager.hyprland.settings = {
+      monitor = [
+        "HDMI-A-3,disable"
+        "DP-3,highrr,auto,1"
+      ];
+    };
   };
 }
