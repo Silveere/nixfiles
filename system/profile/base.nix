4a718a6bef05ed8ba796fb69c3466cbdeec9cfb0
NullBite <me@nullbite.com>
2024-02-29 20:05:28 -0500

    install tmux

--- system/profile/base.nix
+++ system/profile/base.nix
@@ -41,6 +41,7 @@ in
       # $ nix search wget
       environment.systemPackages = with pkgs; [
         neovim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+        tmux
         wget
         curl
         git
