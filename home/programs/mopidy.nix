0f3b7bcba0781dd2405cfbb215eb6ceef89b3945
NullBite <me@nullbite.com>
2024-03-06 02:52:23 -0500

    mopidy: add scrobbler plugin

--- home/programs/mopidy.nix
+++ home/programs/mopidy.nix
@@ -19,6 +19,7 @@ in
         mopidy-bandcamp
         mopidy-ytmusic
         mopidy-soundcloud
+        mopidy-scrobbler
         # outputs.packages.${pkgs.system}.mopidy-autoplay
       ];
     };
