e00ec711a75c54b41227e1778e1a1629b647fc41
NullBite <me@nullbite.com>
2024-03-12 17:43:03 +0000

    Update flake.lock and fix build

--- backports/default.nix
+++ backports/default.nix
@@ -1,10 +1,16 @@
 final: prev:
 let
-  inherit (final) callPackage;
+  inherit (final) callPackage fetchFromGitHub;
   inherit (final.lib) recurseIntoAttrs;
 
-  gimpPlugins-gap = recurseIntoAttrs (callPackage ./gimp-gap.nix {});
+  gimpPlugins-gap = let
+    src = fetchFromGitHub {
+      owner = "Scrumplex";
+      repo = "nixpkgs";
+      rev = "cca25fd345f2c48de66ff0a950f4ec3f63e0420f";
+      hash="sha256-oat4TwOorFevUMZdBFgaQHx/UKqGW7CGMoOHVgQxVdM="; 
+    };
+  in recurseIntoAttrs (callPackage "${src}/pkgs/applications/graphics/gimp/plugins" {});
 in {
-  # apparently it's still broken. see https://github.com/NixOS/nixpkgs/issues/294707#issuecomment-1989857687
-  # gimpPlugins = if prev.gimpPlugins.gap.version == "2.6.0" then gimpPlugins-gap else prev.gimpPlugins;
+  gimpPlugins = if prev.gimpPlugins.gap.version == "2.6.0" then gimpPlugins-gap else prev.gimpPlugins;
 }
