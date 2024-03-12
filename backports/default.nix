15f8f49797e2dbc5068b1e1540069e32e27aa04b
NullBite <me@nullbite.com>
2024-03-11 22:53:10 -0400

    partial revert flake.lock
    gimpPlugins.gap is still broken even with the updated package

--- backports/default.nix
+++ backports/default.nix
@@ -5,5 +5,6 @@ let
 
   gimpPlugins-gap = recurseIntoAttrs (callPackage ./gimp-gap.nix {});
 in {
-  gimpPlugins = if prev.gimpPlugins.gap.version == "2.6.0" then gimpPlugins-gap else prev.gimpPlugins;
+  # apparently it's still broken. see https://github.com/NixOS/nixpkgs/issues/294707#issuecomment-1989857687
+  # gimpPlugins = if prev.gimpPlugins.gap.version == "2.6.0" then gimpPlugins-gap else prev.gimpPlugins;
 }
