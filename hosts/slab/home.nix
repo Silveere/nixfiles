c183023606005684a6262e61758c088bf69a26a4
NullBite <me@nullbite.com>
2024-03-17 20:32:45 -0400

    slab: autostart Hyprland on tty1

--- hosts/slab/home.nix
+++ hosts/slab/home.nix
@@ -1,4 +1,4 @@
-{ lib, pkgs, osConfig, ... }:
+{ lib, pkgs, osConfig, config, ... }:
 {
   imports = [
     ../../home
@@ -9,6 +9,16 @@
       profile.base.enable = true;
     };
     home.stateVersion = "23.11";
+
+    # auto start Hyprland on tty1
+    programs.zsh.initExtraFirst = let
+      hyprland="${config.wayland.windowManager.hyprland.finalPackage}/bin/Hyprland";
+      tty="${pkgs.coreutils}/bin/tty";
+    in lib.mkIf config.wayland.windowManager.hyprland.enable ''
+        if [[ "$(${tty})" == "/dev/tty1" && -z "''${WAYLAND_DISPLAY:+x}" ]] ; then
+          ${hyprland}
+        fi
+      '';
 
     wayland.windowManager.hyprland.settings = {
       monitor = ",preferred,auto,1.25";
