ad3ada835a4cc7a2b56956209d95ed721ffeca80
NullBite <me@nullbite.com>
2024-03-19 04:27:51 -0400

    slab: set up greetd
    i literally just set up an alternative to display managers am i stupid

--- hosts/slab/configuration.nix
+++ hosts/slab/configuration.nix
@@ -46,6 +46,13 @@
   services.xserver.displayManager.sddm.enable = false;
   services.xserver.displayManager.startx.enable = true;
 
+  services.greetd = {
+    enable = true;
+    settings = {
+      default_session.command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --cmd Hyprland --asterisks";
+    };
+  };
+
   nixfiles = {
     profile.pc.enable = true;
     common.remoteAccess.enable = true;
