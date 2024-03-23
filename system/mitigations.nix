01184327533523083ec43ae1b73e49cb87dc5067
NullBite <me@nullbite.com>
2024-03-23 02:58:18 -0400

    Remove Plasma pinentry fix

--- system/mitigations.nix
+++ system/mitigations.nix
@@ -5,14 +5,5 @@ let
 in
 {
   config = lib.mkMerge [
-    (lib.mkIf (p5 || p6) {
-      assertions = [
-        {
-          assertion = ((nixpkgs == inputs.nixpkgs-unstable) && nixpkgs.lastModified < (1710889954 + (60*60*24*2)));
-          message = "workaround still configured in system/mitigations.nix";
-        }
-      ];
-      programs.gnupg.agent.pinentryPackage = lib.mkForce pkgs.pinentry-qt;
-    })
   ];
 }
