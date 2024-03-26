932cf2bb2fc502b83917ed6d759fd06cbe58615e
NullBite <me@nullbite.com>
2024-03-26 19:29:06 -0400

    i tried setting up multi monitor but it scared me

--- hosts/nullbox/home.nix
+++ hosts/nullbox/home.nix
@@ -12,6 +12,11 @@
       monitor = [
         "HDMI-A-3,disable"
         "DP-3,highrr,auto,1"
+        # this is the "proper" multi monitor config but hyprland's multi
+        # monitor system scares me so i am going to keep the other one disabled
+        # still. i might need something called hyprsome or something
+        # "HDMI-A-3,highrr,0x0,1,transform,1"
+        # "DP-3,highrr,1024x160,1"
       ];
     };
   };
