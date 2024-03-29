4ce363374ecd48e963e0a317a000f4641f66b55c
NullBite <me@nullbite.com>
2024-03-29 22:02:47 +0000

    kernel mitigation

--- backports/default.nix
+++ backports/default.nix
@@ -3,6 +3,9 @@ let
   inherit (prev) callPackage fetchFromGitHub;
   inherit (prev.lib) recurseIntoAttrs optionalAttrs;
 
+  xz-hold = nixfiles.inputs.nixpkgs-unstable.legacyPackages.${prev.system}.xz.version == "5.6.1";
+  xz-fixed = (nixfiles.inputs.nixpkgs-staging-next.legacyPackages.${prev.system}.xz);
+
   # if you can't do version based just make it time based and deal with it in a
   # month if it's not fixed
   # 2024-04-10T08:11:11
@@ -15,4 +18,5 @@ let
       hash="sha256-oat4TwOorFevUMZdBFgaQHx/UKqGW7CGMoOHVgQxVdM="; 
     };
   in recurseIntoAttrs (callPackage "${src}/pkgs/applications/graphics/gimp/plugins" {});
-in (optionalAttrs gap-hold { gimpPlugins = gimpPlugins-gap; })
+in (optionalAttrs gap-hold { gimpPlugins = gimpPlugins-gap; }) //
+  (optionalAttrs xz-hold { xz=xz-fixed; })
--- system/mitigations.nix
+++ system/mitigations.nix
@@ -2,8 +2,20 @@
 let
   p5 = config.services.xserver.desktopManager.plasma5.enable;
   p6 = config.services.desktopManager.plasma6.enable;
+
+  isNewer = ref: ver: ((builtins.compareVersions ver ref) == 1);
+
+  # kernel update
+  newKernelPackages = inputs.nixpkgs-staging-next.legacyPackages.${pkgs.system}.linuxPackages_latest;
 in
 {
   config = lib.mkMerge [
+    {
+      boot.kernelPackages = newKernelPackages;
+      assertions = [
+        { assertion = (!(isNewer "6.8" nixpkgs.legacyPackages.${pkgs.system}.linuxPackages.kernel.version));
+          message = "Kernel is no longer outdated. Please remove this."; }
+      ];
+    }
   ];
 }
