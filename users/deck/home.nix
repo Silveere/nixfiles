d2ffb11557616bd3586e81504d304f98d0fa97ef
NullBite <me@nullbite.com>
2024-04-24 22:56:58 -0400

    deck: add update script

--- users/deck/home.nix
+++ users/deck/home.nix
@@ -1,6 +1,13 @@
 { pkgs, config, lib, ... }:
 {
   config = {
+    home.packages = [
+    (pkgs.writeShellScriptBin "hmup" ''
+      unset LD_PRELOAD LD_LIBRARY_PATH
+
+      konsole -e bash -c "nix flake metadata --refresh github:Silveere/nixfiles; nh home switch github:Silveere/nixfiles"
+    '')
+    ];
     programs.keychain.enable = false;
     nixfiles.packageSets.gaming.enable = true;
     nixfiles.packageSets.gaming.enableLaunchers = false;
