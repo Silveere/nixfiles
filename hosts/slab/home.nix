78fd26fd4af7a5dbb76554fe418020b69a99ae7b
NullBite <me@nullbite.com>
2024-05-12 14:44:50 -0400

    slab: document keyboard keys in comments

--- hosts/slab/home.nix
+++ hosts/slab/home.nix
@@ -8,7 +8,11 @@
     nixfiles = {
       profile.base.enable = true;
 
-      common.wm.keybinds.Launch1="playerctl play-pause";
+      common.wm.keybinds = {
+        Launch1="playerctl play-pause"; # ROG key
+        # Launch3="true"; # AURA fn key
+        # Launch4="true"; # fan control fn key
+      };
     };
     home.stateVersion = "23.11";
 
