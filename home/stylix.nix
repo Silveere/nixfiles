19460ea48f278f88ed43f688c4e779f0b78a1e13
NullBite <me@nullbite.com>
2024-05-23 02:45:41 -0400

    Update default config to match stylix#244

--- home/stylix.nix
+++ home/stylix.nix
@@ -1,14 +1,18 @@
 { pkgs, lib, config, inputs, ... }@args:
 {
-  imports = [
-    inputs.stylix.homeManagerModules.stylix
-  ];
+  imports = [ inputs.stylix.homeManagerModules.stylix ];
   config = {
-    # only if styix is standalone
-    stylix = lib.mkIf (!(args ? osConfig && args.osConfig ? stylix )) {
-      autoEnable = lib.mkDefault false;
-      image = lib.mkDefault "${pkgs.nixfiles-assets}/share/wallpapers/nixfiles-static/Djayjesse-finding_life.png";
-      base16Scheme = lib.mkDefault "${pkgs.base16-schemes}/share/themes/catppuccin-mocha.yaml";
-    };
+    stylix = lib.mkMerge [
+      { targets.vim.enable = lib.mkDefault false; }
+      # only if styix is standalone
+      (lib.mkIf (!(args ? osConfig && args.osConfig ? stylix)) {
+        # all of this is documented in system/stylix.nix
+        autoEnable = lib.mkDefault config.stylix.enable;
+        enable = lib.mkDefault false;
+
+        image = lib.mkDefault "${pkgs.nixfiles-assets}/share/wallpapers/nixfiles-static/Djayjesse-finding_life.png";
+        base16Scheme = lib.mkDefault "${pkgs.base16-schemes}/share/themes/catppuccin-mocha.yaml";
+      })
+    ];
   };
 }
--- system/stylix.nix
+++ system/stylix.nix
@@ -1,18 +1,22 @@
-{ pkgs, config, lib, inputs, ... } @ args:
+{ pkgs, config, lib, inputs, ... }@args:
 {
-  imports = [
-    inputs.stylix.nixosModules.stylix
-  ];
+  imports = [ inputs.stylix.nixosModules.stylix ];
 
   config = {
     # stylix defaults (this is an external module so i don't mind setting sane defaults right here).
     stylix = {
-      # don't mess with things by default. note: this still messes with things
-      # by default; the stylix.enable option is currently being added, see
-      # danth/stylix#244
-      autoEnable = lib.mkDefault false;
+      # don't mess with things by default.
+      #
+      # this naming is confusing as shit
+      # this enables color theming of things by stylix
+      autoEnable = lib.mkDefault config.stylix.enable;
+      # this enables installation of fonts/images/etc
+      enable = lib.mkDefault false;
 
+      # an image i like
       image = lib.mkDefault "${pkgs.nixfiles-assets}/share/wallpapers/nixfiles-static/Djayjesse-finding_life.png";
+
+      # default theme
       base16Scheme = lib.mkDefault "${pkgs.base16-schemes}/share/themes/catppuccin-mocha.yaml";
       homeManagerIntegration = {
         # use system config in home-manager
