2c3c68aea5bd49212f35a2ffe7553e0b55de9dd6
NullBite <me@nullbite.com>
2024-06-01 04:31:23 +0000

    wsl: add systemd startup workaround

--- hosts/nixos-wsl/configuration.nix
+++ hosts/nixos-wsl/configuration.nix
@@ -1,4 +1,4 @@
-{ pkgs, config, lib, ... }:
+{ pkgs, config, lib, vars, ... }:
 {
   config = {
     networking.hostName = "nixos-wsl";
@@ -8,6 +8,27 @@
       binfmt.enable = true;
     };
     wsl.interop.register = true;
+
+    users.users.${vars.username}.linger = true;
+    systemd.services = let
+      user = config.users.users.${vars.username};
+      mainUid = builtins.toString user.uid;
+    in {
+      # "user@${mainUid}" = {
+      #   wantedBy = [ "multi-user.target" ];
+      #   overrideStrategy = "asDropin";
+      # };
+      workaround-reisolate = {
+        serviceConfig = {
+          ExecStartPre = "${pkgs.coreutils}/bin/sleep 10";
+          ExecStart = "${pkgs.systemd}/bin/systemctl isolate --no-block default.target";
+          Type = "oneshot";
+          RemainAfterExit = true;
+        };
+        description = "WSL startup workaround";
+        wantedBy = [ "default.target" ];
+      };
+    };
 
     networking.networkmanager.enable = false;
     programs.gnupg.agent = {
