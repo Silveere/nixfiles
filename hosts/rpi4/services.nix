b3b22fe127d94af9cf010c747e2d371c2a855226
NullBite <me@nullbite.com>
2024-06-22 22:11:01 -0400

    rpi4: rearrange services file

--- hosts/rpi4/services.nix
+++ hosts/rpi4/services.nix
@@ -2,12 +2,40 @@
 {
   config = {
 
-    users.groups.secrets = {};
-    users.users.acme.extraGroups = [ "secrets" ];
-
     age.secrets.cloudflaredns = {
       file = ../../secrets/cloudflare-dns.age;
       group = "secrets";
+    };
+
+    users.groups.secrets = {};
+    users.users.acme.extraGroups = [ "secrets" ];
+
+    security.acme = {
+      acceptTerms = true;
+      maxConcurrentRenewals = 1;
+
+      defaults.email = "iancoguz@gmail.com";
+
+      certs = {
+        "protogen.io" = {
+          credentialFiles = {
+            "CLOUDFLARE_EMAIL_FILE" = pkgs.writeText "email" "iancoguz@gmail.com";
+            "CLOUDFLARE_API_KEY_FILE" = config.age.secrets.cloudflaredns.path;
+          };
+
+          dnsProvider = "cloudflare";
+          domain = "protogen.io";
+          extraDomainNames = [
+            "*.protogen.io"
+            "nullbite.com"
+            "*.nullbite.com"
+            "nullbite.dev"
+            "*.nullbite.dev"
+            "nbt.sh"
+            "*.nbt.sh"
+          ];
+        };
+      };
     };
 
     users.users.nginx.extraGroups = [ "acme" ];
@@ -45,32 +73,6 @@
       };
     };
 
-    security.acme = {
-      acceptTerms = true;
-      maxConcurrentRenewals = 1;
-
-      defaults.email = "iancoguz@gmail.com";
-
-      certs = {
-        "protogen.io" = {
-          credentialFiles = {
-            "CLOUDFLARE_EMAIL_FILE" = pkgs.writeText "email" "iancoguz@gmail.com";
-            "CLOUDFLARE_API_KEY_FILE" = config.age.secrets.cloudflaredns.path;
-          };
-
-          dnsProvider = "cloudflare";
-          domain = "protogen.io";
-          extraDomainNames = [
-            "*.protogen.io"
-            "nullbite.com"
-            "*.nullbite.com"
-            "nullbite.dev"
-            "*.nullbite.dev"
-            "nbt.sh"
-            "*.nbt.sh"
-          ];
-        };
-      };
     };
   };
 }
