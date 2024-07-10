b55e2a33717aa71425b7075026f59d4bf6bf432b
NullBite <me@nullbite.com>
2024-07-10 13:13:19 -0400

    Update flake.lock, migrate config

--- hosts/nullbox/mcserver.nix
+++ hosts/nullbox/mcserver.nix
@@ -22,12 +22,12 @@ in
         SUBVOLUME = "/srv/mcserver";
         TIMELINE_CREATE = true;
         TIMELINE_CLEANUP = true;
-        TIMELINE_MIN_AGE = "1800";
-        TIMELINE_LIMIT_HOURLY = "36";
-        TIMELINE_LIMIT_DAILY = "14";
-        TIMELINE_LIMIT_WEEKLY = "4";
-        TIMELINE_LIMIT_MONTHLY = "12";
-        TIMELINE_LIMIT_YEARLY = "10000";
+        TIMELINE_MIN_AGE = 1800;
+        TIMELINE_LIMIT_HOURLY = 36;
+        TIMELINE_LIMIT_DAILY = 14;
+        TIMELINE_LIMIT_WEEKLY = 4;
+        TIMELINE_LIMIT_MONTHLY = 12;
+        TIMELINE_LIMIT_YEARLY = 10000;
       };
     };
 
