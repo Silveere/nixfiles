dbe6810560a2e998b9ff5ad32ad82d83feed754f
NullBite <me@nullbite.com>
2024-07-10 16:59:24 -0400

    flake.nix: update stable branch to 24.05

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "NixOS Configuration";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
                  # ^^^^^^^^^^^^^ this part is optional
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
 
