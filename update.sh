9d4e81fffc973d881cd25f81978cb36d29d4b160
NullBite <me@nullbite.com>
2024-11-19 16:56:16 -0500

    update script: allow only updating pkgs

--- update.sh
+++ update.sh
@@ -3,8 +3,24 @@
 
 set -Eeuxo pipefail
 
+if [[ "$#" -ne 0 ]] ; then
+	until [[ "$#" -le 0 ]]; do
+		case "$1" in
+			pkgs|packages) DO_PACKAGES=1;;
+			flake) DO_FLAKE=1;;
+		esac
+		shift
+	done
+else
+	DO_PACKAGES=1
+	DO_FLAKE=1
+fi
+
 cd "$(dirname "$0")"
 
-nix flake update
 
-nix-update --flake redlib --version=branch=main
+[[ -n "${DO_FLAKE:+x}" ]] && nix flake update || true
+
+if [[ -n "${DO_PACKAGES:+x}" ]] ; then
+	nix-update --flake redlib --version=branch=main
+fi
