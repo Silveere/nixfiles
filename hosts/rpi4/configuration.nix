842651aafb958bb8f3c69611ff941ecf29c46e6d
NullBite <me@nullbite.com>
2024-11-19 19:03:57 -0500

    rpi4: set bootloader configuration limit

--- hosts/rpi4/configuration.nix
+++ hosts/rpi4/configuration.nix
@@ -28,7 +28,10 @@
   # Use the extlinux boot loader. (NixOS wants to enable GRUB by default)
   boot.loader.grub.enable = false;
   # Enables the generation of /boot/extlinux/extlinux.conf
-  boot.loader.generic-extlinux-compatible.enable = true;
+  boot.loader.generic-extlinux-compatible = {
+    enable = true;
+    configurationLimit = 5;
+  };
 
   # temporary while i am away from server
   boot.kernelPackages = pkgs.linuxPackages_6_6;
