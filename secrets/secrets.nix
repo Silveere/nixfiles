3e7e6f0d63e40fa61aec4f8ffc0af5f998b4ada6
NullBite <me@nullbite.com>
2025-01-10 22:07:25 -0500

    secrets: homepage

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -22,6 +22,7 @@ in
   "authelia-storage.age".publicKeys = [ rpi4 ] ++ all-user;
   "authelia-jwt.age".publicKeys = [ rpi4 ] ++ all-user;
   "authelia-session.age".publicKeys = [ rpi4 ] ++ all-user;
+  "homepage.age".publicKeys = [ rpi4 ] ++ all-user;
 
   "restic-rclone.age".publicKeys = [ rpi4 nullbox slab ] ++ all-user;
   "restic-password.age".publicKeys = [ rpi4 nullbox slab ] ++ all-user;
