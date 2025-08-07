3357554780cce9b44c0946110bd7b30713ab6523
NullBite <me@nullbite.com>
2025-08-07 23:31:05 +0200

    rpi4: enable multimedia

--- hosts/rpi4/home.nix
+++ hosts/rpi4/home.nix
@@ -1,6 +1,9 @@
 {config, ...}: {
   config = {
-    nixfiles.profile.base.enable = true;
+    nixfiles = {
+      profile.base.enable = true;
+      packageSets.multimedia.enable = true;
+    };
     programs.keychain.enable = false;
   };
 }
