f12db82de764eaf464d3fadbb320413aab1e80d6
NullBite <me@nullbite.com>
2025-10-01 13:29:51 -0400

    hosts(px1): add home config

--- hosts/nixos-px1/home.nix
+++ hosts/nixos-px1/home.nix
@@ -4,4 +4,11 @@
   lib,
   ...
 }: {
+  config = {
+    nixfiles = {
+      profile.base.enable = true;
+      packageSets.multimedia.enable = true;
+    };
+    programs.keychain.enable = false;
+  };
 }
