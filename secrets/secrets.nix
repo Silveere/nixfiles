338fb45fed74f11a6a64768840d5804f611a98e6
NullBite <me@nullbite.com>
2025-10-07 19:20:38 -0400

    secrets: rekey
    - add standalone key to all secrets to make future rekey easier
    - grant some secrets to nixos-px1

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -12,13 +12,14 @@ let
     "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQC4eg1TMjggwqdur1bsBqz4wzLchxAfcVl9XPJQ/Z4NCjKD+/lqmUQNt1n5ld5w/fRirkcsOIcoWSW1ioisvoZEtv5I5clQLLHA4sNIO2hXnNP3+XF+pB/eBZ6my4/nySl1QkDBEFE7HDTw0S6aZAkk3xoD08W2mU4xcVnVUeBUGyOy2Wt8NESYqwfw0qcIyRd37YmlOk22v/aVlMAsBSI73ug7/qTsVFp8G1py2BLi0ZwA5MfiZ8LZ6Y9gXFVGAA1pi2TiG+PVRMe9HsVHrxYX7crx6XnWmaa2o1KmxqWXriTd8zO2OGgoCW8klIeKnsJ3fGXJlLpcgxOXwg9vkE3EjWyihRrzooQdcfgrdxs7CY4D9OY2HrJ66h/bOtQgQCjI6/jN8SB+thhktj8fF5kSpB2hnquZPddVaRl83EmDqx03eqPVWtoGfEFi5/M6LGcqu8dYVoKioFnMFWaZVxlIn0goNCF6eV4+xPtFn/Wt6o4twYKJVEVXbhFQbiebUgk="
     "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQC0enlNbo1V5q0yq6n90gRPsNznoQ/KLEjeo1yOAUyJwPi35cw+b3p4DRN7T55DcSivKKE9Hyh6bpaQWFJSLyP5jAtDrYkuUfNx5GkgrquMwMvwzk3Z+h2/J/WgDyKQZXtm9LHYTgiW8jDU1lBiks39IqCAGrCTLAmAHSaJ39A4ZpJwu6zZ9sQqT22E/UpFm5MBezdZbm8V0G+beX+y3+pp8Kag7goGNY+rgTgx7REDz3jzZz3FBP+CxKoo1H8HHz78RDqBb8HKpVQYNQkwvIBeczKawRHIkJO2Mk+1mc6Ta6beA9+Uyf+puxco2xl6BOnDInvnhWJIRXOJuR5P8/YWprE1o4ixF2N95D2GlJ618V7faEovu/sNj8qIvfA66OF1gG+LOfNAl+u2+3V8ewATF493F0q04zhenoH1ZdrsACJfL8tK9Ev9056ImR6aSJ5BjqCk0tMmnLKTZ7q3R2LoKnB1r/TXe10OH7rx5BDAt4OmD8a5QS0RvVgK3O/iMW0="
     "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDSAD3bCc9ZHGqU+HoCtp69uU3Px5bbVYYrHbLS3AS1Xku9rPKE/oUU/fz8/AZxp6dLuKhPjQjLzgbFjM0rK57fiOPqBnlw3eAiIymrlB03ALLV3y+GF2OhVf2rkcPkUxjK978dwS9bxgty6WzPAoSjNwilzgf2CcLcHyIzDwwzWCndM9jtpXbUbLhOH6CvsvygBD7j06wakLOorTS+cAFecUvaUkDr6gSu6zpM29DcFiq8T1VoWhBzwC/9IKnxV/XqaZBM3Em7NfPQIzYWcD9A5+Holj2I6jcTSd9xIdMhD4Miqm8IdojPkP2NuVfD9AMxn0ccwbROG/zliyXtcxRj8b3jEquBKuN+yGqF5hexmKlhHmHua9NwhsWnGWjOWWSaPtsp3WOM/fEc7cWpVWZ+W8V5LbdWEY3Ke52Cz35QbOSml09H/gIzsMxZbiZvkJB4PvWKf0FoyZ8ojJWIaGP1/LQdXNMWorqy7tWp3sAw3JcMsR0ezJ3YoI6Y6FOIdL0="
+    agenix
   ];
 
   agenix = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFSjr8JYl9rwU/1b36ndvo37iUx8LVL2PaSQ774eW6Mb";
 in {
-  "cloudflare-dns.age".publicKeys = [rpi4] ++ all-user;
+  "cloudflare-dns.age".publicKeys = [rpi4 px1] ++ all-user;
   "wireguard-rpi4.age".publicKeys = [rpi4] ++ all-user;
-  "htpasswd.age".publicKeys = [rpi4] ++ all-user;
+  "htpasswd.age".publicKeys = [rpi4 px1] ++ all-user;
   "htpasswd-cam.age".publicKeys = [rpi4] ++ all-user;
   "authelia-users.age".publicKeys = [rpi4] ++ all-user;
   "authelia-storage.age".publicKeys = [rpi4] ++ all-user;
@@ -27,8 +28,8 @@ in {
   "homepage.age".publicKeys = [rpi4] ++ all-user;
   "paperless-admin.age".publicKeys = [rpi4] ++ all-user;
 
-  "restic-rclone.age".publicKeys = [rpi4 nullbox slab] ++ all-user;
-  "restic-password.age".publicKeys = [rpi4 nullbox slab] ++ all-user;
+  "restic-rclone.age".publicKeys = [rpi4 nullbox slab px1] ++ all-user;
+  "restic-password.age".publicKeys = [rpi4 nullbox slab px1] ++ all-user;
 
   "anki-user.age".publicKeys = [rpi4] ++ all-user;
 
