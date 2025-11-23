f32c68efa40278b1f6b4a64ebe1715fc1052ced5
NullBite <me@nullbite.com>
2025-11-23 17:21:46 -0500

    system(remote): add ssh hardware key for root
    deploy-rs warns about sudo usage instead of keys (plus it doesn't work
    for some reason anyway)

--- system/common/remote.nix
+++ system/common/remote.nix
@@ -16,6 +16,12 @@ in {
       };
     };
 
+    users.users.root.openssh.authorizedKeys.keys = [
+      # hardware key
+      # (yes it is called "test" i don't care)
+      "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAIISnhXFnXvFlQBDHtf1O2l6kXZPiqZaxXeyMAy6LBwGJAAAABHNzaDo= test"
+    ];
+
     programs.mosh = {
       enable = true;
       openFirewall = true;
