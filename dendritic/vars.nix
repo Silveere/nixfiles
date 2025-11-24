a2c814b3f82b8cdcd5787197dfc6018ba7188392
NullBite <me@nullbite.com>
2025-11-23 19:17:52 -0500

    vars: add ssh keys

--- dendritic/vars.nix
+++ dendritic/vars.nix
@@ -2,12 +2,21 @@
   config,
   lib,
   ...
-}: {
+}: let
+  sshKeys = {
+    tz = "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAIISnhXFnXvFlQBDHtf1O2l6kXZPiqZaxXeyMAy6LBwGJAAAABHNzaDo= test";
+  };
+in {
   config.nixfiles.vars = {
     ### Configuration
     # My username
     username = "nullbite";
     # My current timezone for any mobile devices (i.e., my laptop)
     mobileTimeZone = "America/New_York";
+
+    inherit sshKeys;
+    deployKeys = with sshKeys; [
+      tz
+    ];
   };
 }
