65af91a20907187a6ac7e07f7fcab70139b911f1
NullBite <me@nullbite.com>
2025-11-24 15:28:33 -0500

    ci: line buffer logs

--- ci/logcrap.sh
+++ ci/logcrap.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 filter_logs() {
-    exec grep -ve 'NVIDIA.*found in MITIGATION_RETHUNK build' 2> /dev/null
+    exec grep --line-buffered -ve 'NVIDIA.*found in MITIGATION_RETHUNK build' 2> /dev/null
 }
 
 exec "$@" > >(filter_logs) 2> >(filter_logs >&2)
