7a1258a7943e98920ec9c5a690cb428c409da0ad
NullBite <me@nullbite.com>
2025-12-07 22:49:03 -0500

    flake: add perSystem.*.nixfiles.nvfetcher option
    This is a perSystem option because nvfetcher uses fetchers from `pkgs`.

--- dendritic/nvfetcher.nix
+++ dendritic/nvfetcher.nix
@@ -1,6 +1,32 @@
-{self, ...}: let
+{
+  self,
+  lib,
+  flake-parts-lib,
+  ...
+}: let
   mkNvfetcherSources = pkgs: pkgs.callPackage (self.outPath + "/_sources/generated.nix") {};
+  inherit
+    (flake-parts-lib)
+    mkPerSystemOption
+    ;
+
+  inherit (lib) mkOption;
 in {
+  options.perSystem = mkPerSystemOption ({
+    system,
+    pkgs,
+    ...
+  }: {
+    _file = ./nvfetcher.nix;
+
+    options.nixfiles.nvfetcherSources = mkOption {
+      description = ''
+        nvfetcher sources for system ${system}
+      '';
+      default = mkNvfetcherSources pkgs;
+      readOnly = true;
+    };
+  });
   config = {
     flake.overlays.nvfetcher = final: prev: {
       nvfetcherSources = mkNvfetcherSources final;
