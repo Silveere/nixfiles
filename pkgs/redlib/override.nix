7779de41688ffbbc58e24d3b62fcc62c3db820bd
NullBite <me@nullbite.com>
2025-12-07 22:46:27 -0500

    redlib: patch tls cipher

--- pkgs/redlib/override.nix
+++ pkgs/redlib/override.nix
@@ -9,7 +9,12 @@
 in
   redlib.overrideAttrs (prev: rec {
     inherit (sources.redlib) src pname version;
-    cargoDeps = rustPlatform.importCargoLock sources.redlib.cargoLock."Cargo.lock";
+    # cargoDeps = rustPlatform.importCargoLock sources.redlib.cargoLock."Cargo.lock";
+    cargoDeps = rustPlatform.fetchCargoVendor {
+      inherit src;
+      patches = [./pr-510.patch];
+      hash = "sha256-ageSjIX0BLVYlLAjeojQq5N6/VASOIpwXNR/3msl/p4=";
+    };
 
     checkFlags =
       prev.checkFlags
@@ -24,6 +29,7 @@ in
         # this is so the commit hash can be embedded so redlib doesn't complain
         # about the server being outdated unless it's /actually/ outdated
         ./no-hash.patch
+        ./pr-510.patch
       ];
 
     GIT_HASH = src.rev;
