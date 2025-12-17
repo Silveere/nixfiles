fdec626bad7a57929daf8c7f8e3261979c4f61ff
NullBite <me@nullbite.com>
2025-12-17 13:03:23 +0100

    hosts(meow): update home.nix

--- hosts/nixos-meow/home.nix
+++ hosts/nixos-meow/home.nix
@@ -1,1 +1,14 @@
-{}
+{
+  config,
+  pkgs,
+  lib,
+  ...
+}: {
+  config = {
+    nixfiles = {
+      profile.base.enable = true;
+      packageSets.multimedia.enable = true;
+    };
+    programs.keychain.enable = false;
+  };
+}
