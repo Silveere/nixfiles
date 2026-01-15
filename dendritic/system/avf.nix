9750f64393fe8596950415f785db9a2f54359da3
NullBite <me@nullbite.com>
2026-01-15 17:38:39 -0500

    avf: better uid/gid configuration

--- dendritic/system/avf.nix
+++ dendritic/system/avf.nix
@@ -16,29 +16,30 @@
     config = {
       avf.defaultUser = lib.mkDefault "${vars.username}";
 
-      # revive default user for testing
+      # uid 1000 (droid) is the only user that can access /mnt/shared
       users.users.droid = {
+        uid = 1000;
         isNormalUser = true;
         extraGroups = ["droid" "wheel"];
       };
       users.groups.droid = {};
 
-      # proper uid/gid config
+      # non conflicting uid/gid config
       users.users."${vars.username}" = {
         # slightly above mkDefault
         initialPassword = lib.mkOverride 990 null;
-        uid = 1001;
+        uid = 1010;
       };
       users.groups."${vars.username}" = {
-        gid = 994;
+        gid = 1010;
       };
 
       environment.systemPackages = let
         # mount helper to run it as user `droid` so i can use fstab/systemd mounts
         bindfs-droid = pkgs.writeShellScriptBin "mount.fuse.bindfs.droid" ''
           set -Eeuxo pipefail
-          ${pkgs.coreutils}/bin/chown droid "$2"
-          exec ${pkgs.util-linux}/bin/setpriv --reuid droid --regid droid --init-groups -- ${pkgs.bindfs}/bin/mount.fuse.bindfs "$@"
+          ${pkgs.coreutils}/bin/chown 1000:100 "$2"
+          exec ${pkgs.util-linux}/bin/setpriv --reuid 1000 --regid 100 --init-groups -- ${pkgs.bindfs}/bin/mount.fuse.bindfs "$@"
         '';
       in [
         pkgs.bindfs
