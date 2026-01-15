b3227d81301f7423d7610f5f3877ce6fc35dd9a5
NullBite <me@nullbite.com>
2026-01-15 13:24:28 -0500

    system(nix): don't assume nixpkgs has `rev` attr
    this allows overriding nixpkgs as a path for bootstrapping AVF

--- system/common/nix.nix
+++ system/common/nix.nix
@@ -57,7 +57,7 @@ in {
             path = "${nixpkgs.outPath}";
           };
         };
-        nixpkgs = {
+        nixpkgs = lib.mkIf (nixpkgs ? rev) {
           exact = true;
           from = {
             id = "nixpkgs";
