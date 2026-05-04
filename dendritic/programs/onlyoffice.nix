fa7be95a999a1b7cd9c676a7b108eef705f5d15f
NullBite <me@nullbite.com>
2026-05-04 16:13:55 -0400

    onlyoffice: cleanup, add font

--- dendritic/programs/onlyoffice.nix
+++ dendritic/programs/onlyoffice.nix
@@ -1,9 +1,4 @@
-{
-  config,
-  lib,
-  self,
-  ...
-}: let
+{lib, ...}: let
   fontPackages = pkgs:
     with pkgs; [
       corefonts
@@ -11,6 +6,7 @@
       noto-fonts
       liberation_ttf
       ubuntu-sans
+      atkinson-hyperlegible-next
     ];
 
   fontsPkg = pkgs:
@@ -22,9 +18,6 @@
       cd "$out"
       ${pkgs.gzip}/bin/gunzip -f *.gz
     '';
-  # ${pkgs.mkfontscale}/bin/mkfontscale
-  # ${pkgs.mkfontdir}/bin/mkfontdir
-  # cat $(find ${pkgs.fontalias}/ -name fonts.alias) > fonts.alias
 
   homeModule = {
     config,
@@ -37,27 +30,29 @@
   in {
     options.nixfiles.programs.onlyoffice.enable = lib.mkEnableOption "onlyoffice config";
     config = lib.mkIf cfg.enable {
-      # xdg.dataFile.fonts = {
-      #   recursive = true;
-      #   source = fontsPkg pkgs;
-      # };
       programs.onlyoffice.enable = true;
       home.packages = let
-        find = "${pkgs.findutils}/bin/find";
-        xargs = "${pkgs.findutils}/bin/xargs";
-        bash = pkgs.runtimeShell;
-        cp = "${pkgs.coreutils}/bin/cp";
-        mkdir = "${pkgs.coreutils}/bin/mkdir";
+        esa = lib.escapeShellArg;
+        binPath = with pkgs; "${lib.makeBinPath [coreutils findutils]}:${pkgs.runtimeShell}";
+        scriptSetup = ''
+          set -e
+          PATH=${esa binPath}
+          export PATH
+          XDG_DATA_HOME="''${XDG_DATA_HOME:-$HOME/.local/share}"
+          export XDG_DATA_HOME
+        '';
         copyfonts = pkgs.writeShellScriptBin "onlyoffice-copy-fonts" ''
-          ${mkdir} -p ~/.local/share/fonts
-          ${find} ${(fontsPkg pkgs)} -mindepth 1 -maxdepth 1 -type f -print0 \
-            | ${xargs} -0rn64 ${bash} -c 'exec ${cp} -rf --preserve=timestamps "$@" ~/.local/share/fonts' -
+          ${scriptSetup}
+          mkdir -p "$XDG_DATA_HOME"/fonts
+          find ${esa (fontsPkg pkgs)} -mindepth 1 -maxdepth 1 -type f -print0 \
+            | xargs -0rn64 bash -c 'exec cp -rf --preserve=timestamps -- "$@" "$XDG_DATA_HOME"/fonts/' -
         '';
         # files previously copied with --preserve-timestamps will have the
         # timestamp set to epoch, otherwise this state is very rare.
         # also probably don't do this in production you moron
         rmfonts = pkgs.writeShellScriptBin "onlyoffice-remove-fonts" ''
-          ${find} ~/.local/share/fonts -mindepth 1 -maxdepth 1 -type f -not -newermt @1 -delete
+          ${scriptSetup}
+          find "$XDG_DATA_HOME"/fonts -mindepth 1 -maxdepth 1 -type f -not -newermt @1 -delete
         '';
       in [copyfonts rmfonts];
     };
