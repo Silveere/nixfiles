8399abb4b726832dc2bbe996bc134080b0f06a1f
NullBite <me@nullbite.com>
2026-06-30 15:07:23 -0400

    Update lanzaboote

--- flake.nix
+++ flake.nix
@@ -80,7 +80,7 @@
 
     # https://github.com/nix-community/lanzaboote/releases
     lanzaboote = {
-      url = "github:nix-community/lanzaboote/v0.4.3";
+      url = "github:nix-community/lanzaboote/v1.1.0";
 
       inputs.nixpkgs.follows = "nixpkgs-unstable";
     };
